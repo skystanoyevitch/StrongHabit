9e51e595e339b457587438b0c960e2cecfd50519
Sky Stanoyevitch <skystanoyevitch@proton.me>
2025-02-11 11:06:05 -0800

    Add custom theme configuration for React Native Paper

---
+++ src/constants/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from "react-native-paper";
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#007AFF", // iOS blue
+    accent: "#FF2D55",
+  },
+};
