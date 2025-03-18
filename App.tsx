5ed32ad960596b5d7e17eb4c9f38a5456f8c50ca
Sky Stanoyevitch <skystanoyevitch@proton.me>
2025-03-18 06:32:19 -0700

    feat: integrate push notifications and register for notifications on app launch

--- App.tsx
+++ App.tsx
@@ -3,8 +3,23 @@ import "react-native-reanimated";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import TabNavigator from "./src/navigation/TabNavigator";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
+import { registerForPushNotificationsAsync } from "./src/utils/notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+  }, []);
+
   return (
     <NavigationContainer>
       <TabNavigator />
