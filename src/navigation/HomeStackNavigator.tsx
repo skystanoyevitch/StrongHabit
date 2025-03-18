5709666550dd90eee0c7766da910a6138ddf4448
Sky Stanoyevitch <skystanoyevitch@proton.me>
2025-03-18 11:01:41 -0700

    feat: add StatsScreen to HomeStackNavigator and update screen options

--- src/navigation/HomeStackNavigator.tsx
+++ src/navigation/HomeStackNavigator.tsx
@@ -3,15 +3,21 @@ import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
 import HabitDetailScreen from "../screens/HabitDetailScreen";
 import EditHabitScreen from "../screens/EditHabitScreen";
+import StatsScreen from "../screens/StatsScreen";
 
 const Stack = createStackNavigator();
 
 export default function HomeStackNavigator() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false, // This adds or removes all headers
+      }}
+    >
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="HabitDetail" component={HabitDetailScreen} />
       <Stack.Screen name="EditHabit" component={EditHabitScreen} />
+      <Stack.Screen name="Stats" component={StatsScreen} />
     </Stack.Navigator>
   );
 }
