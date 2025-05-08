4319b9c469eefb8e8e97dc08b1af62bd0eb291d2
Sky Stanoyevitch <skystanoyevitch@proton.me>
2025-05-07 18:06:41 -0700

    feat: add SettingsStackNavigator for navigation in settings section

---
+++ src/navigation/SettingsStackNavigator.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import SettingsScreen from "../screens/SettingsScreen";
+import BackupScreen from "../screens/BackupScreen";
+import AutoBackupSettings from "../screens/AutoBackupSettings";
+import CloudBackupScreen from "../screens/CloudBackupScreen";
+
+const Stack = createStackNavigator();
+
+export default function SettingsStackNavigator() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
+      <Stack.Screen name="Backup" component={BackupScreen} />
+      <Stack.Screen name="AutoBackupSettings" component={AutoBackupSettings} />
+      <Stack.Screen name="CloudBackup" component={CloudBackupScreen} />
+    </Stack.Navigator>
+  );
+}
