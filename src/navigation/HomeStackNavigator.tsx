1b877fcf95933e6756cf9026eb2a7748bd5b9a41
Sky Stanoyevitch <skystanoyevitch@proton.me>
2025-05-07 18:06:34 -0700

    feat: add missing screens to HomeStackNavigator for improved navigation

--- src/navigation/HomeStackNavigator.tsx
+++ src/navigation/HomeStackNavigator.tsx
@@ -4,6 +4,9 @@ import HomeScreen from "../screens/HomeScreen";
 import HabitDetailScreen from "../screens/HabitDetailScreen";
 import EditHabitScreen from "../screens/EditHabitScreen";
 import StatsScreen from "../screens/StatsScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+import BackupScreen from "../screens/BackupScreen";
+import AutoBackupSettings from "../screens/AutoBackupSettings";
 
 const Stack = createStackNavigator();
 
@@ -18,6 +21,9 @@ export default function HomeStackNavigator() {
       <Stack.Screen name="HabitDetail" component={HabitDetailScreen} />
       <Stack.Screen name="EditHabit" component={EditHabitScreen} />
       <Stack.Screen name="Stats" component={StatsScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+      <Stack.Screen name="Backup" component={BackupScreen} />
+      <Stack.Screen name="AutoBackupSettings" component={AutoBackupSettings} />
     </Stack.Navigator>
   );
 }
