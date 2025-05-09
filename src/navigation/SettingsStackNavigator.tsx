0223457b2c8cd5302787aeba9cbdf4e162f98e34
Sky Stanoyevitch <skystanoyevitch@proton.me>
2025-05-08 19:11:37 -0700

    feat: add TimezoneSettings screen to SettingsStackNavigator

--- src/navigation/SettingsStackNavigator.tsx
+++ src/navigation/SettingsStackNavigator.tsx
@@ -4,6 +4,7 @@ import SettingsScreen from "../screens/SettingsScreen";
 import BackupScreen from "../screens/BackupScreen";
 import AutoBackupSettings from "../screens/AutoBackupSettings";
 import CloudBackupScreen from "../screens/CloudBackupScreen";
+import TimezoneSettingsScreen from "../screens/TimezoneSettingsScreen";
 
 const Stack = createStackNavigator();
 
@@ -18,6 +19,10 @@ export default function SettingsStackNavigator() {
       <Stack.Screen name="Backup" component={BackupScreen} />
       <Stack.Screen name="AutoBackupSettings" component={AutoBackupSettings} />
       <Stack.Screen name="CloudBackup" component={CloudBackupScreen} />
+      <Stack.Screen
+        name="TimezoneSettings"
+        component={TimezoneSettingsScreen}
+      />
     </Stack.Navigator>
   );
 }
